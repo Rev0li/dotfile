9013f490b606b2bd707ae8ae5a68ba379b0ff3e2
Oliver Kientzler <okientzl@z3r6p5.42lyon.fr>
2025-09-22 11:49:48 +0200

    update nvim and lsp-cmp

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -2,13 +2,9 @@
 # Configuration Oh My Zsh et plugins
 
 # 💻 Oh My Zsh
-export ZSH="$HOME/.oh-my-zsh"
 ZSH_THEME="eastwood"
 plugins=(
     git 
     zsh-autosuggestions 
     zsh-syntax-highlighting
 )
-
-# Charger Oh My Zsh
-source $ZSH/oh-my-zsh.sh
